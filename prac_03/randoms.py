b15567b4d393a17cc6094519c6564a76d62972ff
Muhammad Musawir <muhammad.musawir@my.jcu.edu.au>
2023-11-22 17:12:58 +0800

    Add password check program

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.randint(5, 20))  # line 1 >>> 5 and 20
+print(random.randrange(3, 10, 2))  # line 2 >>> 3 and 9. 4 cannot be produced
+print(random.uniform(2.5, 5.5))  # line 3 >>>2.551... and 5.495...
+print(random.randint(1, 100))
