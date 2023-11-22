9c177ebf78ee7b105d10ba81bab8d6da3c6f1a5b
Muhammad Musawir <muhammad.musawir@my.jcu.edu.au>
2023-11-22 16:46:46 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-password =input("Password:")
-while len(password) < 4:
-    print("Password should be atleast 4 characters")
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end='')
+
+
+def get_password():
     password = input("Password:")
-for i in range(len(password)):
-    print("*", end='')
+    while len(password) < 4:
+        print("Password should be atleast 4 characters")
+        password = input("Password:")
+    return password
+
+
+main()
